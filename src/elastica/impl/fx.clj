75fb45a02f5a2dabfd363d4707822244aaec94fa
Tom Goldsmith <tom.goldsmith@7theta.com>
2018-09-13 17:20:40 -0400

    Request body prepped for indexing properly.

--- src/elastica/impl/fx.clj
+++ src/elastica/impl/fx.clj
@@ -26,61 +26,65 @@
                   :request (:opts %)
                   :es-error (:body %)})))
 
+(defn- intercept-before
+  [context]
+  (if-let [http (:http context)]
+    (update context :http
+            (fn [http]
+              (-> http
+                  (update :url (fsafe
+                                (fn [url]
+                                  (if (string? url)
+                                    url
+                                    (ec/url
+                                     (:cluster url)
+                                     :indices (if (coll? (:indices url))
+                                                (map ->es-key (:indices url))
+                                                (->es-key (:indices url)))
+                                     :type (->es-key (:type url))
+                                     :segments (:segments url)
+                                     :query (->> url :query ->es (map-vals ->es-key)))))))
+                  (update :body (fsafe (comp json/write-value-as-string ->es)))
+                  (update :es-> #(or % es->)))))
+    context))
+
+(defn- intercept-after
+  [context]
+  (if-let [http (:http context)]
+    (if-let [verb ({:put http/put
+                    :post http/post
+                    :get http/get
+                    :delete http/delete
+                    :head http/head
+                    :options http/options} (:method http))]
+      (let [result (ec/promise)]
+        (verb
+         (str (:url http))
+         (compact (dissoc http :url :method :es-> :response-xform))
+         (fn [response]
+           (let [es-> (:es-> http)
+                 response (update
+                           response :body
+                           (comp (fsafe
+                               #(es-> (json/read-value % json-mapper)))
+                              not-empty))
+                 response-xform
+                 (or (:response-xform http)
+                     (fn [{:keys [status headers body error]}]
+                       (if (#{200 201} status)
+                         body
+                         (ex-info "HTTP Error"
+                                  {:status status
+                                   :request (dissoc http :url :es-> :response-xform)
+                                   :response response
+                                   :es-error (:body response)}))))]
+             (deliver result (response-xform response)))))
+        (assoc context :http/result result))
+      (throw (ex-info "Unrecognized HTTP verb" {:http http})))
+    context))
+
 (def http-interceptor
   (->interceptor
    :id :elastica.impl.coercion/es-interceptor
-   :before
-   (fn [context]
-     (if-let [http (:http context)]
-       (update context :http
-               (fn [http]
-                 (-> http
-                     (update :url (fsafe
-                                   (fn [url]
-                                     (if (string? url)
-                                       url
-                                       (ec/url
-                                        (:cluster url)
-                                        :indices (if (coll? (:indices url))
-                                                   (map ->es-key (:indices url))
-                                                   (->es-key (:indices url)))
-                                        :type (->es-key (:type url))
-                                        :segments (:segments url)
-                                        :query (->> url :query ->es (map-vals ->es-key)))))))
-                     (update :body (fsafe (comp ->es json/write-value-as-string)))
-                     (update :es-> #(or % es->)))))
-       context))
-   :after
-   (fn [context]
-     (if-let [http (:http context)]
-       (if-let [verb ({:put http/put
-                       :post http/post
-                       :get http/get
-                       :delete http/delete
-                       :head http/head
-                       :options http/options} (:method http))]
-         (let [result (ec/promise)]
-           (verb
-            (str (:url http))
-            (compact (dissoc http :url :method :es-> :response-xform))
-            (fn [response]
-              (let [es-> (:es-> http)
-                    response (update
-                              response :body
-                              (comp (fsafe
-                                  #(es-> (json/read-value % json-mapper)))
-                                 not-empty))
-                    response-xform
-                    (or (:response-xform http)
-                        (fn [{:keys [status headers body error]}]
-                          (if (#{200 201} status)
-                            body
-                            (ex-info "HTTP Error"
-                                     {:status status
-                                      :request (dissoc http :url :es-> :response-xform)
-                                      :response response
-                                      :es-error (:body response)}))))]
-                (deliver result (response-xform response)))))
-           (assoc context :http/result result))
-         (throw (ex-info "Unrecognized HTTP verb" {:http http})))
-       context))))
+   :before (fn [context] (intercept-before context))
+   :after (fn [context] (intercept-after context))))
--- src/elastica/query/aggregations.clj
+++ src/elastica/query/aggregations.clj
@@ -1,61 +1,92 @@
 (ns elastica.query.aggregations
-  (:require [utilis.map :refer [compact]]))
+  (:require [elastica.impl.coercion :refer [->es-key]]
+            [utilis.map :refer [compact map-vals]]))
+
+;;; Declarations
+
+(declare xform-field)
+
+;;; Public
 
 (defn significant-terms
   [field & {:keys [name] :or {name :significant_terms_agg}}]
   {:pre [field name]}
-  {name {:significant_terms {:field field}}})
+  (xform-field {name {:significant_terms {:field field}}}))
 
 (defn terms
   [field & {:keys [name] :or {name :terms_agg}}]
-  {name {:terms {:field field}}})
+  (xform-field {name {:terms {:field field}}}))
 
 (defn date-histogram
   [field & {:keys [name interval]
             :or {name :date_histogram_agg
                  interval :month}}]
-  {name
-   {:date_histogram
-    {:field field
-     :interval interval}}})
+  (xform-field
+   {name
+    {:date_histogram
+     {:field field
+      :interval interval}}}))
 
 (defn significant-text
   [field & {:keys [name filter-duplicate-text]
             :or {name :significant_text_agg}}]
   {:pre [field name]}
-  (compact
-   {name
-    {:significant_text
-     {:field field
-      :filter_duplicate_text filter-duplicate-text}}}))
+  (xform-field
+   (compact
+    {name
+     {:significant_text
+      {:field field
+       :filter_duplicate_text filter-duplicate-text}}})))
 
 (defn sampler
   [& {:keys [shard-size name aggregations]
       :or {shard-size 100
            name :sampler_agg}}]
-  (compact
-   {name
-    {:sampler {:shard_size shard-size}
-     :aggregations aggregations}}))
+  (xform-field
+   (compact
+    {name
+     {:sampler {:shard_size shard-size}
+      :aggregations aggregations}})))
 
 (defn geo-centroid
   [& {:keys [name field]
       :or {name :centroid}}]
-  {name {:geo_centroid {:field field}}})
+  (xform-field
+   {name {:geo_centroid {:field field}}}))
 
 (defn geohash-grid
   [& {:keys [name field precision size]
       :or {name :grid}}]
-  (compact
-   {name
-    {:geohash_grid
-     {:field field
-      :precision precision
-      :size size}}}))
+  (xform-field
+   (compact
+    {name
+     {:geohash_grid
+      {:field field
+       :precision precision
+       :size size}}})))
 
 (defn bounded-geohash-grid
   [& {:keys [name geohash-grid bounding-box]
       :or {name :bounded-grid}}]
-  {name
-   {:filter bounding-box
-    :aggregations geohash-grid}})
+  (xform-field
+   {name
+    {:filter bounding-box
+     :aggregations geohash-grid}}))
+
+;;; Private
+
+(defn- xform-field
+  [m]
+  (cond
+
+    (map? m)
+    (->> m
+         (map (fn [[k v]] [k (if (= :field k) (->es-key v) v)]))
+         (into {})
+         (map-vals xform-field))
+
+    (coll? m)
+    (->> m (map xform-field)
+         (into (empty m)))
+
+    :else m))
